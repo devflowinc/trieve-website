651d33db11f2abddea7443f87855b10c48cd7c8d
Sara Vieira <hey@iamsaravieira.com>
2024-09-09 10:03:29 +0100

    feat: use new dataset with our docs

--- src/components/widgets/Search.jsx
+++ src/components/widgets/Search.jsx
@@ -2,8 +2,8 @@ import { initTrieveModalSearch, TrieveSDK } from 'trieve-search-component';
 import 'trieve-search-component/dist/index.css';
 
 const trieve = new TrieveSDK({
-  apiKey: 'tr-l1IRx4Jw0iVICiFdf9NroFwmDWQ4CnEd',
-  datasetId: '85bdeb65-44ec-4c9c-9d64-725601ad672a',
+  apiKey: 'tr-cs4zCkeKgrki09aOykgKUt53K2Uv0mhT',
+  datasetId: '15bd032c-ff24-454d-beee-6b6cac97d8be',
 });
 
 initTrieveModalSearch({
